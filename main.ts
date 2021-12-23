5bfd5bb102d57a6b6c3d954129c1e91f4116984b
joha2738 <65940893+joha2738@users.noreply.github.com>
2021-12-23 22:42:01 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    speed = 100
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.analogWritePin(AnalogPin.P2, 75)
+    speed = 10
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    speed = 0
+})
+let speed = 0
+speed = 1023
+led.setBrightness(50)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    if (speed == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (speed == 10) {
+        led.toggle(0, 2)
+    } else if (speed == 100) {
+        led.plot(0, 2)
+    } else {
+    	
+    }
+})
